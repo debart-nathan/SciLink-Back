f39b8dee28258654230aacb0a41fb1c2e2da538c
Benoit Gueheneux <benoit.gueheneux@hotmail.com>
2023-11-14 21:23:55 +0100

    fix-and-comment-practices-controller

--- src/Controller/PracticesController.php
+++ src/Controller/PracticesController.php
@@ -14,39 +14,47 @@ class PracticesController extends AbstractController
     #[Route('/Practices', name: 'app_practices')]
     public function index(Request $request, ResearchersRepository $researchersRepository, DomainsRepository $domainsRepository): JsonResponse
     {
+        // Vérifie s'il y a des paramètres de requête
         if ($request->query->count() > 0) {
             $queryParams = $request->query->all();
+            // Vérifie si l'identifiant du rechercheur est présent dans les paramètres
             if (isset($queryParams['researcher_id'])) {
                 $researcherId = $queryParams['researcher_id'];
             }
+            // Vérifie si l'identifiant du domaine est présent dans les paramètres
             if (isset($queryParams[''])) {
                 $domainId = $queryParams['domain_id'];
             }
         }
         $practicesArray = [];
-        if (isset($researcherId) && isset($domainId)) {
-            $researcher = $researchersRepository->find($researcherId);
-        }
+        // Si l'identifiant du chercheur est spécifié
         if (isset($researcherId)) {
             $researcher = $researchersRepository->find($researcherId);
             $domains = $researcher->getDomains();
             foreach ($domains as $domain) {
+                // Construit le tableau des pratiques pour le chercheur et ses domaines
                 $practicesArray[] = [
                     'researcher_id' => $researcherId,
                     'domain_id' => $domain->getId(),
                 ];
             }
-        } elseif (isset($domainId)) {
+        }
+        // Si l'identifiant du domaine est spécifié
+        elseif (isset($domainId)) {
             $domain = $domainsRepository->find($domainId);
             $researchers = $domain->getResearchers();
+            // Construit le tableau des pratiques pour le domaine et ses chercheurs
             foreach ($researchers as $researcher) {
                 $practicesArray[] = [
                     'researcher_id' => $researcher->getId(),
                     'domain_id' => $domainId,
                 ];
             }
-        } else {
+        }
+        // Si aucun identifiant spécifié, récupère toutes les pratiques
+        else {
             $researchers = $researchersRepository->findAll();
+            // Construit le tableau des pratiques pour tous les chercheurs et leurs domaines
             foreach ($researchers as $researcher) {
                 $domains = $researcher->getDomains();
                 foreach ($domains as $domain) {
@@ -57,6 +65,7 @@ class PracticesController extends AbstractController
                 }
             }
         }
+        // Convertit le tableau en JSON et retourne une réponse JSON
         $practicesJson = json_encode($practicesArray);
         return new JsonResponse($practicesJson, 200, [], true);
     }
